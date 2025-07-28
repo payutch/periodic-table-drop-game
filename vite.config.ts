999d16bc9b32e1d7a38293cfda43bb209e422c85
payutch <payuthch@gmail.com>
2025-07-29 00:13:58 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
+import { fileURLToPath } from 'url';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
